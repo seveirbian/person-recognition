917d86e421523d88a3ba084b42a0c7c56c6b822a
seveirbian <33503641+seveirbian@users.noreply.github.com>
2018-06-12 17:05:54 +0800

    Create camera-server.py

---
+++ camera-server/camera-server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+# comment this out if you're not using USB webcam
+from camera_opencv import Camera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world!"
+
+def gen2(camera):
+    """Returns a single image frame"""
+    frame = camera.get_frame()
+    yield frame
+
+@app.route('/image.jpg')
+def image():
+    """Returns a single current image for the webcam"""
+    return Response(gen2(Camera()), mimetype='image/jpeg')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True)
