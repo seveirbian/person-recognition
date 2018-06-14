38bd5266576c963dbe32960781d8254f8e5c0c99
seveirbian <33503641+seveirbian@users.noreply.github.com>
2018-06-14 15:30:28 +0800

    add some comments.

--- camera-server/camera-server.py
+++ camera-server/camera-server.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+# Camera Server
+# 
+# @Author: Bian
+# @Abstract: Camera Server runs in a container at port 5000 of the host, 
+# takes pictures and sends them to the port.
+# @Date: 2018.06.14
+
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
