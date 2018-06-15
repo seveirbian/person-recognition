080670ceccf30801c01ef1115399c2a2e92ca4e3
seveirbian <33503641+seveirbian@users.noreply.github.com>
2018-06-15 10:21:38 +0800

    change 127.0.0.1 to 0.0.0.0

--- camera-server/start-serving.sh
+++ camera-server/start-serving.sh
@@ -5,6 +5,6 @@
 # @Date: 2018.06.14
 
 docker run -d --name server --rm \
-        -p 127.0.0.1:5000:5000 \
+        -p 0.0.0.0:5000:5000 \
         --device /dev/video0:/dev/video0 \
         seveirroy/camera-server
