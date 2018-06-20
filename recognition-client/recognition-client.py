c3fb02e0266a21932b13395f18a90f6a1bb3fefa
seveirbian <33503641+seveirbian@users.noreply.github.com>
2018-06-20 11:08:59 +0800

    add argment

--- recognition-client/recognition-client.py
+++ recognition-client/recognition-client.py
@@ -3,7 +3,7 @@
 # @Author: Bian
 # @Abstract: Recognition Client gets pictures from Camera Server, 
 # detects them and hightlights the detected persons.
-# @Date: 2018.06.14
+# @Date: 2018.06.20
 
 from darkflow.net.build import TFNet
 import cv2
@@ -13,11 +13,13 @@ import requests
 import tkinter as tk
 from PIL import Image, ImageTk, ImageDraw
 import numpy as np
+import sys
 
 def recognition():
+    global server_addr
     global pic_num
 
-    r = requests.get('http://localhost:5000/image.jpg')
+    r = requests.get("http://"+server_addr+"/image.jpg")
     curr_img = Image.open(BytesIO(r.content))
     curr_img_cv2 = cv2.cvtColor(np.array(curr_img), cv2.COLOR_RGB2BGR)
 
@@ -69,16 +71,22 @@ def window():
         open_jpg = Image.open(pic_name)
         bm = ImageTk.PhotoImage(image=open_jpg)
         label.configure(image=bm)
-        label.after(10, changeImage)
+        label.after(100, changeImage)
 
-    label.after(10, changeImage)
+    label.after(100, changeImage)
     main_window.mainloop()
         
 
 if __name__ == "__main__":
     # import model and load weights
+    global server_addr
     global pic_num
     pic_num = 0
+
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("you should only input on argv: server_addr,\nlike: 192.168.1.1:5000\n")
+        sys.exit()
+    server_addr = sys.argv[1]
 
     options = {"model": "darkflow/cfg/tiny-yolo-voc.cfg", "load": "darkflow/bin/tiny-yolo-voc.weights", "threshold": 0.1}
     tfnet = TFNet(options)
